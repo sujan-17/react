afe80f39c1bcc9300b4ab51541890ee6503fec27
sujan-17 <sujan17062006@gmail.com>
2025-02-04 15:17:52 +0530

    Added new homepage logo

--- h/src/pages/home.jsx
+++ h/src/pages/home.jsx
@@ -1,8 +1,10 @@
+import logo from "../assets/logo.png";
 const Home = () => {
     return (
       <div className="hero">
         <h1>Welcome to Our Website</h1>
         <p>Experience the best of our services with a modern touch.</p>
+        <img src={logo} alt="company logo" />
       </div>
     );
   };
